0da46cdd053ef3d0991c62e3d3aab6aa2a589ecb
Dominic Taylor <dominic@dominictaylor.co.uk>
2017-05-02 14:25:20 +0100

    add one-page skeleton

---
+++ alpine-grav-one-page/files/run.sh
@@ -0,0 +1,37 @@
+#!/bin/ash
+
+[ -f /run-pre.sh ] && /run-pre.sh
+
+if [ ! -d /usr/html ] ; then
+  mkdir -p /usr/html
+  chown -R nginx:nginx /usr/html
+else
+  chown -R nginx:nginx /usr/html
+fi
+
+if [ ! -d /usr/html/system ] ; then
+  curl -fLk -o /tmp/grav.zip  "https://getgrav.org/download/skeletons/onepage-site/1.0.2"
+  mkdir /tmp/grav-src
+  unzip /tmp/grav.zip -d /tmp/grav-src
+  mv -f /tmp/grav-src/grav-admin/* /usr/html/
+  rm -R /tmp/grav*
+  chown -R nginx:nginx /usr/html
+else
+  chown -R nginx:nginx /usr/html
+fi
+
+chown -R nginx:nginx /usr/html
+
+find /usr/html -type f | xargs chmod 664
+find /usr/html -type d | xargs chmod 775
+find /usr/html -type d | xargs chmod +s
+
+# start php-fpm
+mkdir -p /usr/logs/php-fpm
+php-fpm7
+
+# start nginx
+mkdir -p /usr/logs/nginx
+mkdir -p /tmp/nginx
+chown nginx /tmp/nginx
+nginx
