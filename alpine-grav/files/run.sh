45e954ba3e20ccf33bc7e3c2cc2d43a62f34f6bb
fabrizio2210 <fabrizio2210@gmail.com>
2019-03-09 08:32:42 +0100

    transform from deliver to admin for x86_64

---
+++ alpine-grav/files/run.sh
@@ -0,0 +1,13 @@
+#!/bin/ash
+
+[ -f /run-pre.sh ] && /run-pre.sh
+
+# start php-fpm
+mkdir -p /usr/logs/php-fpm
+php-fpm7
+
+# start nginx
+mkdir -p /usr/logs/nginx
+mkdir -p /tmp/nginx
+chown nginx /tmp/nginx
+nginx
